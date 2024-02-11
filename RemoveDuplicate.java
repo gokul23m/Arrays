94a746b4454f5ff11c8a74149695707621135a5d
gokul23m <gokul23m2001@gmail.com>
2024-02-11 21:16:03 +0530

    first commit

---
+++ RemoveDuplicate.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class RemoveDuplicate{
+       public static void main(String[] args){
+               Scanner sc=new Scanner(System.in);
+               System.out.println("Enter the size of array:");
+               int n =sc.nextInt();
+               int arr[]=new int[n];
+                 
+
+               for(int i=0;i<n;i++){            
+                   arr[i]=sc.nextInt();
+               }
+                
+       }
+}
